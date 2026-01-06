fcc3c110eac7002b16f4d7b7d482b492937894c0
willionacle <willionacle@gmail.com>
2026-01-06 21:42:16 +0800

    update

--- src/pages/home/hero/logo-3d.tsx
+++ src/pages/home/hero/logo-3d.tsx
@@ -1,49 +1,49 @@
 import { Canvas, useLoader, useFrame } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, Environment, ContactShadows } from "@react-three/drei";
 import { SVGLoader } from "three/examples/jsm/loaders/SVGLoader.js";
 import * as THREE from "three";
-import { useLayoutEffect, useRef } from "react";
+import { useLayoutEffect, useMemo, useRef } from "react";
 import { isMobile } from "react-device-detect";
 
-const GradientMaterial = () => {
-  const material = new THREE.MeshPhongMaterial({
-    shininess: 1000,
-    specular: new THREE.Color(1, 1, 1),
-  });
+// const GradientMaterial = () => {
+//   const material = new THREE.MeshPhongMaterial({
+//     shininess: 1000,
+//     specular: new THREE.Color(1, 1, 1),
+//   });
 
-  material.onBeforeCompile = (shader) => {
-    shader.fragmentShader = shader.fragmentShader.replace(
-      `#include <dithering_fragment>`,
-      `
-      vec3 topColor = vec3(0.75, 0.08, 0.08);
-      vec3 bottomColor = vec3(0.75, 0.08, 0.08);
+//   material.onBeforeCompile = (shader) => {
+//     shader.fragmentShader = shader.fragmentShader.replace(
+//       `#include <dithering_fragment>`,
+//       `
+//       vec3 topColor = vec3(0.75, 0.08, 0.08);
+//       vec3 bottomColor = vec3(0.75, 0.08, 0.08);
 
-      float grad = smoothstep(-1.0, 1.0, vViewPosition.y);
-      vec3 gradient = mix(bottomColor, topColor, grad);
+//       float grad = smoothstep(-1.0, 1.0, vViewPosition.y);
+//       vec3 gradient = mix(bottomColor, topColor, grad);
 
-      vec3 viewDir = normalize(vViewPosition);
-      vec3 n = normalize(normal);
+//       vec3 viewDir = normalize(vViewPosition);
+//       vec3 n = normalize(normal);
 
-      float cavity = 1.0 - max(dot(viewDir, n), 0.0);
-      cavity = pow(cavity, 6.2);
+//       float cavity = 1.0 - max(dot(viewDir, n), 0.0);
+//       cavity = pow(cavity, 6.2);
 
-      float fresnel = pow(1.0 - max(dot(n, viewDir), 0.0), 2.5);
-      vec3 edgeShine = vec3(1.0, 0.6, 0.6) * fresnel * 0.85;
+//       float fresnel = pow(1.0 - max(dot(n, viewDir), 0.0), 2.5);
+//       vec3 edgeShine = vec3(1.0, 0.6, 0.6) * fresnel * 0.85;
 
-      float specBoost = pow(max(dot(reflect(-viewDir, n), viewDir), 0.0), 16.0);
-      vec3 specularHighlight = vec3(1.0) * specBoost * 0.35;
+//       float specBoost = pow(max(dot(reflect(-viewDir, n), viewDir), 0.0), 16.0);
+//       vec3 specularHighlight = vec3(1.0) * specBoost * 0.35;
 
-      vec3 finalColor =
-        gradient + edgeShine + specularHighlight;
+//       vec3 finalColor =
+//         gradient + edgeShine + specularHighlight;
 
-      gl_FragColor = vec4(finalColor, 1.0);
-      #include <dithering_fragment>
-    `
-    );
-  };
+//       gl_FragColor = vec4(finalColor, 1.0);
+//       #include <dithering_fragment>
+//     `
+//     );
+//   };
 
-  return material;
-};
+//   return material;
+// };
 
 function Logo3D() {
   const svgData = useLoader(SVGLoader, "/svg-logo.svg");
@@ -51,17 +51,29 @@ function Logo3D() {
   const pivotRef = useRef<THREE.Group>(null);
   const logoRef = useRef<THREE.Group>(null);
 
-  const shapes: THREE.Shape[] = [];
-  svgData.paths.forEach((path) => {
-    shapes.push(...SVGLoader.createShapes(path));
-  });
+  const shapes = useMemo(() => {
+    return svgData.paths.flatMap((path) => SVGLoader.createShapes(path));
+  }, [svgData]);
 
-  const material = GradientMaterial();
+  // const material = GradientMaterial();
+
+  const material = useMemo(
+    () =>
+      new THREE.MeshStandardMaterial({
+        color: "#f84543",
+        metalness: 1,
+        roughness: 0.3, // Shiny silver
+        envMapIntensity: 1.5,
+      }),
+    []
+  );
 
   const meshes = shapes.map((shape, i) => {
     const geometry = new THREE.ExtrudeGeometry(shape, {
       depth: 60,
       bevelEnabled: true,
+      bevelThickness: 3,
+      bevelSize: 2,
       curveSegments: 32,
     });
 
@@ -132,11 +144,18 @@ export default function Scene() {
       camera={{ position: [0, 0, 100], fov: 40 }}
       className="-top-52 lg:-top-[30%] w-full h-dvh absolute!"
     >
+      <Environment preset="city" />
       <ambientLight intensity={0.3} />
       <directionalLight position={[10, 20, 20]} intensity={4} />
 
       <Logo3D />
-
+      <ContactShadows
+        position={[0, -15, 0]}
+        opacity={0.1}
+        scale={100}
+        blur={2}
+        far={20}
+      />
       <OrbitControls enableZoom={false} rotateSpeed={0.8} />
     </Canvas>
   );
